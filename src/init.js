bd1f7010aaa724a5d2a31c2fde37aa09a9792103
zhiyuan.guo <zhiyuan.guo@bjrxtd.com>
2021-04-24 15:58:27 +0800

    feat:模板的查找顺序

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 import { initState } from './state'
-function initMinin(Vue){
+export  function initMinin(Vue){
    
     Vue.prototype._init = function(options){
         const vm = this;
@@ -7,10 +7,30 @@ function initMinin(Vue){
 
         // 初始化状态（讲数据做一个初始化的劫持 当数据改变更新视图）
         // 对数据进行初始化 watch computed props data
-        initState(vm)
-    }
-}
+        initState(vm);
 
-export {
-    initMinin
+        if(vm.$options.el){
+            vm.$mount(vm.$options.el)
+        }
+    }
+
+    Vue.prototype.$mount = function(el){
+        el = document.querySelector(el);
+        const vm = this;
+        const options = vm.$options;
+        // 三种挂载方式
+        // 1) render 有render直接使用
+        // 2) template 没有render看template
+        // 3) 最后是el找html 找外部模板
+        if(!options.render){
+            let template = options.template;
+            if(!template && el){
+                template = el.outerHTML;
+            }
+            console.log(template)
+            // 如何将模板编译成函数
+            const render = compileToFunctions(template);
+            options.render = render; // 保证render一定有
+        }
+    }
 }
